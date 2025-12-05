95537b4ae605fa4a2a5ca553630220e4ac3e54bc
GeorgeBacalu <georgebacalu83@gmail.com>
2025-12-05 13:03:30 +0200

    [Feature] Add cors for frontend access

--- FitnessTracker.Api/Program.cs
+++ FitnessTracker.Api/Program.cs
@@ -1,10 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options => options.AddPolicy("Cors", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().WithExposedHeaders("X-Correlation-ID", "Content-Disposition")));
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
+app.UseCors("Cors");
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
