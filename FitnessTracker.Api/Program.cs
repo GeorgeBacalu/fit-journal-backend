a6786e8a5e31a7655bf63a24b24656e0012e33cc
GeorgeBacalu <georgebacalu83@gmail.com>
2025-12-16 00:39:25 +0200

    [Feature] Add serilog

--- FitnessTracker.Api/Program.cs
+++ FitnessTracker.Api/Program.cs
@@ -1,6 +1,12 @@
 using FitnessTracker.Api;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, config)
+    => config.WriteTo.Console()
+             .ReadFrom.Configuration(context.Configuration));
+
 var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
 
